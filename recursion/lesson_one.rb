dbb3b5a1bb995a59ad5a261828bcc3fb9929f7e7
Roy Nyaga <nyagaandreroy@gmail.com>
2020-04-19 20:04:28 +0100

    add more comments to lesson one of recursion

--- recursion/lesson_one.rb
+++ recursion/lesson_one.rb
@@ -2,16 +2,18 @@
 #instances of the same problem(as opposed to iteration). The approach can be applied to many types of problems and
 # recursion is one of the central ideas of computer science.
 
+# A recursive function could also be defined as a function that calls itself until a solution is found.
+
 # In this exercise we are going to create a function which calculates the factorial of a certain number.
 # example 5! = 1 * 2 * 3 * 4 * 5
 
 def factorial number
-  if number <= 1
+  if number <= 1 # Base case: this is the stopping condition
     number
   else
-    number * factorial(number-1)
+    number * factorial(number-1) # recursive call
   end
 end
 
 p factorial 5
-p 1 * 2 * 3 * 4 * 5
+# 120
