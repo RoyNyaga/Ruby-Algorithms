f5cf714bd543b324cfa43b0f609d6e143b1141f8
andre-roy <andre@vdbapp.com>
2021-09-30 23:32:21 +0100

    commit of the day

--- test1.rb
+++ test1.rb
@@ -7,10 +7,29 @@
 #   csv << body
 # end 
 
-[1,2,3,4,5,6].each do |n|
-  if n != 4
-    puts "n is less"
-  else
-    return puts "quited"
-  end 
+# [1,2,3,4,5,6].each do |n|
+#   if n != 4
+#     puts "n is less"
+#   else
+#     return puts "quited"
+#   end 
+# end 
+
+# a = false 
+# c = false
+# if a or c
+#   b = false 
+# end 
+
+# p b
+
+"asdf,".split(",").each do |s|
+  puts s
 end 
+
+"ho , are , you    ".split(",").each do |v|
+  v.strip!
+  p v
+end 
+
+
